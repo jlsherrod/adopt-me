e482e7d652f6f05ca393a7ef75bd757bd6097a39
James Sherrod <jlsherrod@gmail.com>
2022-01-02 14:26:51 -0600

    feat: initial version of Adopt Me project from Frontend Masters
    This is a video course from Brian Holt. Worked on it with Wassim as
    our introduction to react.
    
    References:
    https://btholt.github.io/complete-intro-to-react-v6/

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,31 @@
+//mostly took this from react docs
+import { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
+      return (
+        <h2>
+          Ah man, this listing has an error.
+          <Link to="/">Click here to head home or wait 5 seconds.</Link>
+        </h2>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
